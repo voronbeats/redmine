20cf9b6e7568cfcddcb1ccc73d2af8e1af7ea2bf
дима <hooliganada01@gmail.com>
2023-06-07 16:42:43 +0300

    ipi54_1 добавление редактора к коментариям

--- frontend/widget/Com/views/index.php
+++ frontend/widget/Com/views/index.php
@@ -1,4 +1,5 @@
 <?php
+use vova07\imperavi\Widget;
 use yii\widgets\Pjax;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
@@ -12,7 +13,33 @@ use yii\widgets\ActiveForm;
     <div class="alert alert-success" role="alert">Комментарий сохранен</div>
 <?}else{?>
     <?php $form = ActiveForm::begin(['options' => ['data-pjax' => true]]); ?>
-    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'text')->widget(Widget::className(), [
+        'settings' => [
+            'lang' => 'ru',
+            'minHeight' => 200,
+            'formatting' => [
+                'h1',
+                'h2',
+                'p',
+                'blockquote'
+            ],
+            'plugins' => [
+                'clips',
+                'fullscreen',
+                'video',
+                'fontcolor',
+                'fontfamily',
+                'fontsize',
+            ],
+            'imageUpload' => \yii\helpers\Url::to(['/ajax/save-redactor-img', 'sub' => 'article']),
+            'imageDelete' => \yii\helpers\Url::to(['/ajax/save-img-del']),
+            'clips' => [
+                ['Красный', '<span class="label-red">Здесь вставить текст</span>'],
+                ['Зеленый', '<span class="label-green">Здесь вставить текст</span>'],
+                ['Голубой', '<span class="label-blue">Здесь вставить текст</span>'],
+            ],
+        ],
+    ])->label('Текст') ?>
     <?= $form->field($model, 'task_id')->hiddenInput(['value' => Yii::$app->request->get('id')])->label(false) ?>
     <div class="form-group">
         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
