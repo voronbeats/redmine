0c7bd46716ebec04be33a9cf4d68b2146bc8c3c8
Гриша <grishavladimirov2009@gmail.com>
2023-06-09 13:31:03 +0300

    ipi31 Закомментировать надписи в подвале

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -91,8 +91,8 @@ AppAsset::register($this);
 
 <footer class="footer mt-auto py-3 text-muted">
     <div class="container">
-        <p class="float-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-        <p class="float-right"><?= Yii::powered() ?></p>
+        <!--<p class="float-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+        <p class="float-right"><?= Yii::powered() ?></p>-->
     </div>
 </footer>
 
