3237b41612a72722f0aecd6a503e4e6c86e220a2
Гриша <grishavladimirov2009@gmail.com>
2023-06-09 17:56:27 +0300

    ipi77 Поправить Иконки В Шапке

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -51,21 +51,19 @@ AppAsset::register($this);
                         </ul>
                       </div>
                       <ul class="rd-navbar-nav">
-                        <li class="rd-nav-item"><i class="fa fa-home" aria-hidden="true"></i><a class="rd-nav-link" href="/">Домой</a></li>
-                        <li class="rd-nav-item"><a class="rd-nav-link" href="/labor-costs/statics">Статистика</a></li>
+                        <li class="rd-nav-item"><i class="fa fa-home" aria-hidden="true"></i> <a class="rd-nav-link" href="/">Домой</a></li>
+                        <li class="rd-nav-item"><i class="fa fa-bar-chart" aria-hidden="true"></i> <a class="rd-nav-link" href="/labor-costs/statics">Статистика</a></li>
                         <? if (!Yii::$app->user->isGuest) { ?>
                           <li class="rd-nav-item">
-                            <i class="fa fa-user"></i>
-                            <a style="margin-left: 5px; margin-right: 5px;" class="rd-nav-link" href="/task/user">Мои задачи</a>
+                          <i class="fa fa-user"></i> <a style="margin-left: 5px; margin-right: 5px;" class="rd-nav-link" href="/task/user">Мои задачи</a>
                           </li>
                        <? } ?>
                        
-                        <li class="rd-nav-item"><i class="fa fa-pencil" aria-hidden="true"></i><a class="rd-nav-link" href="/task/create">Создание задачи</a></li>
-                        <li class="rd-nav-item"><i class="fa fa-book" aria-hidden="true"></i><a class="rd-nav-link" href="/labor-costs">Создание трудозатрат</a></li>
+                        <li class="rd-nav-item"><i class="fa fa-pencil" aria-hidden="true"></i> <a class="rd-nav-link" href="/task/create">Создание задачи</a></li>
+                        <li class="rd-nav-item"><i class="fa fa-book" aria-hidden="true"></i> <a class="rd-nav-link" href="/labor-costs">Создание трудозатрат</a></li>
                         <li class="rd-nav-item">
                         <? if (!Yii::$app->user->isGuest) { ?>
-                          <i class="fa fa-share"></i>
-                                <a><?= Html::a(
+                          <i class="fa fa-share"></i>  <a><?= Html::a(
                                     'Выход',
                                     ['/site/logout'],
                                     ['data-method' => 'post', 'class' => 'rd-nav-link']
