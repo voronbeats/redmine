ef0725d1e6492f531c2aa0785f0107d3209759c4
дима <hooliganada01@gmail.com>
2023-06-13 15:21:57 +0300

    ipi83 При создании задачи в поле Родительская задача. Убрать поиск только по своим задачам.

--- frontend/modules/SearchTasks/controllers/DefaultController.php
+++ frontend/modules/SearchTasks/controllers/DefaultController.php
@@ -16,7 +16,7 @@ class DefaultController extends Controller
      */
     public function actionIndex($text)
     {   
-        $models = Task::find()->where(['id'=>$text])->orWhere(['LIKE','name',$text])->andWhere(['user_id' => Yii::$app->user->id])->all();
+        $models = Task::find()->where(['id'=>$text])->orWhere(['LIKE','name',$text])->all();
         $this->layout = false;
         return $this->render('index', compact('models'));
     }
