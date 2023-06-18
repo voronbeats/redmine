8d15e826d3c484459287002dcb4aae9fd445a1c8
Anton <web-spiker@mail.ru>
2023-06-18 22:24:07 +0300

    Правка

--- frontend/modules/SearchTasks/views/default/index.php
+++ frontend/modules/SearchTasks/views/default/index.php
@@ -1,7 +1,7 @@
 <? if($models) {?>
     <ul>
         <? foreach($models as $res) {?>
-            <li><span class="click-search" data-id="<?=$res->id?>">[<?=$res->id?>] <?=$res->name?> (<?=$res->author->username?>)</span></li>
+            <li><span class="click-search" data-id="<?=$res->id?>">#<?=$res->id?> <?=$res->name?> (<?=$res->author->username?>)</span></li>
         <? } ?>
     </ul>
 <? }else{ ?>
