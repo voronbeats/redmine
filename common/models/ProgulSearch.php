841eb719d91eaeeb361abc7951fa26acc99e3e57
Smirnovv19 <hooliganada01@gmail.com>
2023-07-13 11:12:10 +0300

    ipi184 Вывести в обратном порядке записи в прогулах

--- common/models/ProgulSearch.php
+++ common/models/ProgulSearch.php
@@ -46,6 +46,9 @@ class ProgulSearch extends Progul
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort' => [
+                'defaultOrder' => ['id' => SORT_DESC],
+               ]
         ]);
 
         $this->load($params);
